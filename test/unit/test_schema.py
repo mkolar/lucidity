cd07041acf2c6a1651d7e75ce8b37238487ce42a
Roy Nieterau <roy_nieterau@hotmail.com>
2015-05-24 01:14:26 +0200

    Set up draft schema design for templates with nesting and optional parameters.
    - Add test for schema with optional parameters (not passing yet)

--- test/unit/test_schema.py
+++ test/unit/test_schema.py
@@ -14,8 +14,8 @@ TEST_TEMPLATE_PATH = os.path.join(
     os.path.dirname(__file__), '..', 'fixture', 'template'
 )
 
-TEST_FIXTURE_ROOT = os.path.join(
-    os.path.dirname(__file__), '..', 'fixture'
+TEST_SCHEMA_ROOT = os.path.join(
+    os.path.dirname(__file__), '..', 'fixture', 'schema'
 )
 
 @pytest.fixture
@@ -33,11 +33,43 @@ def test_schema():
     assert isinstance(schema, lucidity.Schema)
 
 
-def test_schema_from_yaml():
-    '''Valid initializing from yaml'''
-    schema = lucidity.Schema.from_yaml(os.path.join(TEST_FIXTURE_ROOT, 'schema.yaml'))
-
-
 def test_schema_from_yaml_simple():
+    '''Valid initializing from yaml without nesting'''
+    schema = lucidity.Schema.from_yaml(os.path.join(TEST_SCHEMA_ROOT, 'schema_simple.yaml'))
+
+
+@pytest.mark.parametrize(('template_id', 'data', 'expected'), [
+    ('optional1',   # #1 (1/1 optionals)
+     {'project': {'name': 'foobar'}, 'variation': 'evil'},
+     'foobar/art/var_evil/concept'),
+    ('optional1',   # #1 (0/1 optionals)
+     {'project': {'name': 'foobar'}},
+     'foobar/art/concept'),
+    ('optional2',   # #2 (0/2 optionals)
+     {'project': {'name': 'swag'}, 'asset': {'name': 'dude'}, 'version': 1912},
+     'swag/model/dude_v1912.mb'),
+    ('optional2',   # #2 (1/2 optionals)
+     {'project': {'name': 'transformers'}, 'asset': {'name': 'lolly'}, 'version': 471, 'variation': 'blue'},
+     'transformers/model/lolly_blue_v0471.mb'),
+    ('optional2',   # #2 (2/2 optionals)
+     {'project': {'name': 'foobar'}, 'asset': {'name': 'johny'}, 'date': '20150601', 'version': 5, 'variation': 'red'},
+     'foobar/model/johny_20150601_red_v0005.mb'),
+    ], ids=[
+            '#1 (1/1 optionals)',
+            '#1 (0/1 optionals)',
+            '#2 (0/2 optionals)',
+            '#2 (1/2 optionals)',
+            '#2 (2/2 optionals)'
+    ])
+def test_schema_from_yaml_optional(template_id, data, expected):
     '''Valid initializing from yaml'''
-    schema = lucidity.Schema.from_yaml(os.path.join(TEST_FIXTURE_ROOT, 'schema_simple.yaml'))
+    schema = lucidity.Schema.from_yaml(os.path.join(TEST_SCHEMA_ROOT, 'schema_optional.yaml'))
+
+    template = schema.get_template(template_id)
+    path = template.format(data)
+    assert path == expected
+
+
+def test_schema_from_yaml_nested():
+    '''Valid initializing from yaml'''
+    schema = lucidity.Schema.from_yaml(os.path.join(TEST_SCHEMA_ROOT, 'schema_nested.yaml'))
